2c29eb295c52103bb585e447e120eb5a55d49eab
balasahebgulave <43171791+balasahebgulave@users.noreply.github.com>
2018-09-25 16:58:40 +0530

    Update Indian_Names_data_preprocessing.py

--- Indian_Names_data_preprocessing.py
+++ Indian_Names_data_preprocessing.py
@@ -1,18 +1,23 @@
 import pandas as pd
 
+# Indian-Male-Names.csv and Indian-Female-Names.csv contains raw csv of Indian names.
+
 male_names = pd.read_csv('Indian-Male-Names.csv')
+# male_names contains data available in Indian-Male-Names.csv which contain only unprocessed male names.
 
 female_names = pd.read_csv('Indian-Female-Names.csv')
+# female_names contains data available in Indian-Female-Names.csv which contain only unprocessed female names.
 
 namelist = []
 
 for names in female_names['name']:
-
+# processing on names acailable in data.
     first_name = str(names).strip().split(' ')[0]
 
     namelist.append(first_name)
 
 for names in male_names['name']:
+# processing on names acailable in data.
 
     first_name = str(names).strip().split(' ')[0]
 
@@ -21,8 +26,10 @@ for names in male_names['name']:
 processed_name_list = []
 
 s = 'abcdefghijklmnopqrstuvwxyz'
+# s contains all alphabates a-z.
 
 for i in namelist:
+# processing on names acailable in namelist which contains all male and female names .
 
     i = i.split('@')[0]
 
@@ -38,11 +45,18 @@ for i in namelist:
 
             if j in s:
                 processed_name_list.append(i)
+ 
 
 unique_names = set(processed_name_list)
+# unique_names contains unique names and removes repeated names.
 
 processed_name_list = sorted(list(unique_names))
+# processed_name_list contains list of all names in sorted format.
 
 data = pd.DataFrame(processed_name_list, columns=['Name'])
+# data contains dataframe of processed_name_list.
 
 data.to_csv('Indian_Names.csv')
+# final output create Indian_Names.csv
+
+
